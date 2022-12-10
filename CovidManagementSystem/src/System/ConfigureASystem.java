10ab6a749a7d4b42f77922fc62f5cfee557a3f7f
Grishma <karkera.g@northeastern.edu>
2022-12-10 12:03:05 -0500

    System

---
+++ CovidManagementSystem/src/System/ConfigureASystem.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package System;
+
+
+import System.Employee.Employee;
+import System.Role.SystemAdminRole;
+import System.UserAccount.UserAccount;
+/**
+ *
+ * @author infin
+ */
+public class ConfigureASystem {
+    
+    public static EcoSystem configure(){
+        
+        EcoSystem system = EcoSystem.getInstance();
+        
+        //Create a network
+        //create an enterprise
+        //initialize some organizations
+        //have some employees 
+        //create user account
+        
+        
+        Employee employee = system.getEmployeeDirectory().createEmployee("RRH");
+        
+        UserAccount ua = system.getUserAccountDirectory().createUserAccount("sysadmin", "sysadmin", employee, new SystemAdminRole());
+        
+        return system;
+    }
+    
+}
